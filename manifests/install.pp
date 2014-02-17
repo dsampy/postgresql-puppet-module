c5dbcfe2d707341a78c35e9a9521b1d51960d972
Derrick Ampy <dsampy@gmail.com>
2014-02-17 17:33:26 -0500

    Updated install Puppet classes to download artifacts

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,3 +1,8 @@
 class postgresql::install {
-
+	wget::fetch { "Download PostgreSQL":
+	  source      => 'http://oscg-downloads.s3.amazonaws.com/packages/postgres-9.3.2-2.x86_64.openscg.rpm',
+	  destination => '/tmp/postgres-9.3.2-2.rpm',
+	  timeout     => 0,
+	  verbose     => false,
+	}
 }

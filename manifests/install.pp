a53bff734bdf8e01cc2a05ba97f8de57ec4c70fb
Derrick Ampy <dsampy@gmail.com>
2014-02-17 20:26:27 -0500

    Updated Puppet install classes to use nexus repository

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 class postgresql::install {
 	wget::fetch { "Download PostgreSQL":
-	  source      => 'http://oscg-downloads.s3.amazonaws.com/packages/postgres-9.3.2-2.x86_64.openscg.rpm',
+	  source      => 'http://10.0.2.2:8081/nexus/service/local/repositories/public/content/org/postgresql/postgresql/9.3.2-2/postgresql-9.3.2-2.rpm',
 	  destination => '/tmp/postgres-9.3.2-2.rpm',
 	  timeout     => 0,
 	  verbose     => false,
